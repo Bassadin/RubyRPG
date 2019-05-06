1e88200fe3567a4d226897c168de4144c3d2555d
Bastian Hodapp <bastianhodapp@gmail.com>
2019-05-06 16:04:50 +0200

    Add health management

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    //Movement and collision
     public float moveSpeed = 3.0f;
     Rigidbody2D rigidbody2D;
+
+    //Health
+    public int startHealth = 5;
+    int currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        currentHealth = startHealth;
     }
 
     // Update is called once per frame
@@ -25,5 +31,11 @@ public class RubyController : MonoBehaviour
         position.y += moveSpeed * vertical * Time.deltaTime;
 
         rigidbody2D.MovePosition(position);
+    }
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, startHealth);
+        Debug.Log(currentHealth + " / " + startHealth);
     }
 }
