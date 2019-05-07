c59dcfa36143235588a93153b1424710739f3b0e
Bastian Hodapp <bastianhodapp@gmail.com>
2019-05-07 11:08:26 +0200

    started bullet

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    Rigidbody2D rigidbody2D;
+
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Launch(Vector2 direction, float force)
+    {
+        rigidbody2D.AddForce(direction * force);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Projectile Collision with " + collision.gameObject);
+        Destroy(gameObject);
+    }
+}
